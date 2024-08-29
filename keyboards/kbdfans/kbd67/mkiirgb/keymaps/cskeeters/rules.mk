179a1a32387d3902e088158472e3fb9b0db02849
Chad Skeeters <chad.skeeters@gmail.com>
2024-08-29 16:02:55 -0500

    Disables console for kbd67 just to save RAM for registers

--- keyboards/kbdfans/kbd67/mkiirgb/keymaps/cskeeters/rules.mk
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/cskeeters/rules.mk
@@ -3,5 +3,5 @@ AVR_USE_MINIMAL_PRINTF = no    # Use less capable printf - Size:   400
 REGISTER_ENABLE = yes
 
 # Override keyboard defaults
-CONSOLE_ENABLE = yes           # Console for debug
+CONSOLE_ENABLE = no            # Console for debug
 DYNAMIC_MACRO_ENABLE = no      # Enabled rec/playback    - Size: 1,162
